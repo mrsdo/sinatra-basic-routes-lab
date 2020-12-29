4cbf8c058be969f50ac4c69c2a4d82cb34e2fbe6
Martinique Dolce <me@martidolce.com>
2020-12-29 04:27:33 -0500

    Spec's passed
    Submitting assignment

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
+# frozen_string_literal: true
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get('/name') do
+    'My name is Marti'
+  end
+  get('/hometown') do
+    'My hometown is Florida'
+  end
+  get('/favorite-song') do
+    'My favorite song is Lift Every Voice'
+  end
 end
+
